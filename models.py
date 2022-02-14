aab2bb86850d5cacad991c6c2f12e5847a6eea39
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-14 14:14:08 +0100

    aktualizacja modeli - zmiana klasy Field

--- models.py
+++ models.py
@@ -3,6 +3,34 @@
 import abc
 
 
+class Field:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.letter_dictionary = {'A': 1, 'B': 2, 'C': 3, 'D': 4, "E": 5, "F": 6, 'G': 7, 'H': 8}
+        self.is_empty = True
+        self.figure_code = 0
+        self.field_name = self.count_field_name()
+
+    def check_is_correct(self) -> bool:
+        if 0 <= self.x <= 8 and 0 <= self.y <= 8:
+            return True
+        else:
+            return False
+
+    def change_to_numbers(self, letter):
+        return self.letter_dictionary[letter]
+
+    def change_to_letters(self, number):
+        number += 65
+        letter = chr(number)
+        return letter
+
+    def count_field_name(self):
+        letter = self.change_to_letters(self.y)
+        return letter + self.x
+
+
 class Figure(abc.ABC):
     """
     metoda list_available_moves() zwraca dostęne ruchy
