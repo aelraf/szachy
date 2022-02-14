ba6c2f26a916f2f9e24219314a2cc182579dcede
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-14 13:46:36 +0100

    Klasy figur do models.py

--- models.py
+++ models.py
@@ -20,3 +20,75 @@ class Figure(abc.ABC):
     @abc.abstractmethod
     def validate_move(self):
         pass
+
+
+class King(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 100
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
+
+
+class Queen(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 10
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
+
+
+class Rook(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 5
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
+
+
+class Bishop(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 4
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
+
+
+class Knight(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 3
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
+
+
+class Pawn(Figure):
+    def __init__(self, field):
+        super().__init__(field)
+        self.value = 1
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self):
+        pass
