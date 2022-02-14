60b1a14e5a914db3b39441fe5f9ca4618862e116
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-14 13:24:10 +0100

    dodanie do widoków klasy Field

--- views.py
+++ views.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
+# https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+
 
 import abc
 
 
-class Figure(abc.ABC):
-    """
-
-    """
-
-    @abc.abstractmethod
+class Field:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def check_is_correct(self) -> bool:
+        if 0 <= self.x <= 8 and 0 <= self.y <= 8:
+            return True
+        else:
+            return False
+
+    def change_to_numbers(self):
+        dictionary = {''}
+
+
+class Figure(abc.ABC):
+    """
+    metoda list_available_moves() zwraca dostęne ruchy
+    metoda validate_move(dest_field) informuje, czy ruch na wskazane pole jest możliwy
+    """
+
+    @abc.abstractmethod
+    def __init__(self, field):
+        self.field = field
 
     @abc.abstractmethod
     def list_available_moves(self):
