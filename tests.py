eed8e9a456d429624e986c83378689b1deaec072
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-15 11:43:14 +0100

    testy - inicjacja klasy Field

--- tests.py
+++ tests.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 # https://pypi.org/project/pytest/
+
+from flask.testing import Client
+import pytest
+
+from models import Field
+
+
+class TestsModelFields:
+    def test_fields_init_only_cord(self):
+        field = Field(2, 2)
+
+        assert field.figure_code == 0
+        assert field.is_empty
+        assert field.field_name == 'B2'
+        assert not field.is_black
