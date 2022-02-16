09fc12abab97d11cb061277a79bcb322e0bd50e0
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-16 14:44:37 +0100

    Bishop - prawo góra

--- models.py
+++ models.py
@@ -251,6 +251,24 @@ class Bishop(Figure):
                         break
                 if xk < 1 or yk < 1:
                     break
+            print("prawo góra: ")
+            for i in range(1, 8):
+                yk = y + i
+                xk = x + i
+                print("i: {}, xk: {}, yk: {}".format(i, xk, yk))
+                if 1 <= xk <= 8 and 1 <= yk <= 8:
+                    if fields is not None and fields[xk + yk * 8].is_empty:
+                        list_moves.append(Field(xk, yk))
+                    elif fields is None:
+                        list_moves.append(Field(xk, yk))
+                    elif fields is not None and not fields[xk + yk * 8].is_empty:
+                        break
+                if xk < 1 or yk < 1:
+                    break
+
+            print('lewo dół: ')
+
+            print('prawo dół: ')
 
         except IndexError as err:
             print('list_available_moves - Index Error: {}'.format(err))
