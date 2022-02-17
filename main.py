809641374851d86d7d3ad1a0826ba3f11d9192e1
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-17 18:16:02 +0100

    ChessMove - metoda get + poprawki reszty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse, abort
+
+from .models import Field, King, Queen, Bishop, Knight, Rook, Pawn
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,18 +11,50 @@ def abort_if_field_doesnt_exist(field: str):
     letters = {'A': 1, 'B': 2, 'C': 3, 'D': 4, "E": 5, "F": 6, 'G': 7, 'H': 8}
     if field[0] not in letters or int(field[1:]) > 8 or int(field[1:]) < 1:
         abort(409, message="Field does not exist.")
+    return None
 
 
 def abort_if_figure_doesnt_exist(figure: str):
     figures = {'king', 'queen', 'bishop', 'knight', 'rook', 'pawn'}
     if figure not in figures:
         abort(404, message="Bad figures.")
+    return None
+
+
+def change_to_numbers(letter: str) -> int:
+    try:
+        letters = {'A': 1, 'B': 2, 'C': 3, 'D': 4, "E": 5, "F": 6, 'G': 7, 'H': 8}
+        return letters[letter]
+    except KeyError as err:
+        print("change_to_number: {}, KeyError: {}".format(letter, err))
+        raise KeyError
 
 
 class ChessMove(Resource):
     def get(self, figure: str, current_field: str):
-        abort_if_field_doesnt_exist(field=current_field)
-        abort_if_figure_doesnt_exist(figure=figure)
+        error = abort_if_field_doesnt_exist(field=current_field)
+        error = abort_if_figure_doesnt_exist(figure=figure)
+
+        field = Field(int(current_field[1]), change_to_numbers(current_field[0]))
+
+        dict = {
+            'king': King(field),
+            'queen': Queen(field),
+            'bishop': Bishop(field),
+            'knight': Knight(field),
+            'rook': Rook(field),
+            'pawn': Pawn(field)
+        }
+
+        checking_figure = dict.get(figure, '')
+        available_moves = checking_figure.list_available_moves(field)
+
+        return jsonify({
+            'availableMoves': available_moves,
+            'error': error,
+            'figure': figure,
+            'currentField': current_field
+        })
 
 
 class ChessCheck(Resource):
