c9dc38efa762d2e961b26976a1ab9204b5808996
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-17 20:07:10 +0100

    ChessMove - get działa zgodnie z wytycznymi

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ api = Api(app)
 def abort_if_field_doesnt_exist(field: str):
     letters = {'A': 1, 'B': 2, 'C': 3, 'D': 4, "E": 5, "F": 6, 'G': 7, 'H': 8}
     if field[0] not in letters or int(field[1:]) > 8 or int(field[1:]) < 1:
-        abort(409, message="Field does not exist.")
-    return None
+        # abort(409, error="Field does not exist.")
+        return 409, "Field does not exist."
+    return 200, None
 
 
 def abort_if_figure_doesnt_exist(figure: str):
     figures = {'king', 'queen', 'bishop', 'knight', 'rook', 'pawn'}
     if figure not in figures:
-        abort(404, message="Bad figures.")
-    return None
+        # abort(404, error="Bad figures.")
+        return 404, "Bad figures."
+    return 200, None
 
 
 def change_to_numbers(letter: str) -> int:
@@ -32,37 +34,44 @@ def change_to_numbers(letter: str) -> int:
 
 class ChessMove(Resource):
     def get(self, figure: str, current_field: str):
-        error = abort_if_field_doesnt_exist(field=current_field)
-        error = abort_if_figure_doesnt_exist(figure=figure)
+        error1_code, error1_message = abort_if_field_doesnt_exist(field=current_field)
+        error2_code, error2_message = abort_if_figure_doesnt_exist(figure=figure)
+        available_moves = []
 
-        field = Field(int(current_field[1]), change_to_numbers(current_field[0]), is_empty=False)
-        print(field.field_name)
-        dict = {
-            'king': King(field),
-            'queen': Queen(field),
-            'bishop': Bishop(field),
-            'knight': Knight(field),
-            'rook': Rook(field),
-            'pawn': Pawn(field)
-        }
+        if error1_code == 200 and error2_code == 200:
+            field = Field(int(current_field[1]), change_to_numbers(current_field[0]), is_empty=False)
+            print(field.field_name)
+            dict = {
+                'king': King(field),
+                'queen': Queen(field),
+                'bishop': Bishop(field),
+                'knight': Knight(field),
+                'rook': Rook(field),
+                'pawn': Pawn(field)
+            }
 
-        checking_figure = dict.get(figure, '')
-        field.figure_code = checking_figure.value
+            checking_figure = dict.get(figure, '')
+            field.figure_code = checking_figure.value
 
-        print('GET - figura: {}, pole: {},sprawdzane: {}'.format(
-            checking_figure.value,
-            checking_figure.field.field_name,
-            field.field_name)
-        )
+            print('GET - figura: {}, pole: {},sprawdzane: {}'.format(
+                checking_figure.value,
+                checking_figure.field.field_name,
+                field.field_name)
+            )
 
-        available_moves = checking_figure.list_available_moves()
+            available_moves = checking_figure.list_available_moves()
+            print(available_moves)
+
+        error_code = error1_code if error1_code != 200 else error2_code if error2_code != 200 else None
+        error_message = error1_message if error1_code else error2_message
+        response_code = 200 if error1_code is None and error2_code is None else error_code
 
         return make_response(jsonify({
-            'availableMoves': [available_moves],
-            'error': error,
+            'availableMoves': available_moves,
+            'error': error_message,
             'figure': figure,
             'currentField': current_field
-        }))
+        }), response_code)
 
 
 class ChessCheck(Resource):
